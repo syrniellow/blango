743b8a491a33a50dba61867b1225fb8ed6e41d96
Michele Cernigliaro <cerniello@gmail.com>
2023-05-31 23:04:13 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -57,6 +57,7 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        'blango_auth',
         'blog',
         'crispy_forms',       # forms support library
         'crispy_bootstrap5',  # forms support library
@@ -201,6 +202,8 @@ class Dev(Configuration):
     ]
 
     INTERNAL_IPS = ["192.168.10.226"]
+
+    AUTH_USER_MODEL = "blango_auth.User"
     
 class Prod(Dev):
     DEBUG = False
