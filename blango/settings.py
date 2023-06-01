66e33bfeae8d6b2b2754f395fa2315f5a1dc42f8
Michele Cernigliaro <cerniello@gmail.com>
2023-06-01 22:46:25 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -204,7 +204,12 @@ class Dev(Configuration):
     INTERNAL_IPS = ["192.168.10.226"]
 
     AUTH_USER_MODEL = "blango_auth.User"
-    
+
+    # email 
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+    #REGISTRATION_OPEN = False
+
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
