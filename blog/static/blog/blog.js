201b2f861697863c8e3590880f12130d4ca22128
Michele Cernigliaro <cerniello@gmail.com>
2023-06-25 18:15:58 +0000

    Finish ReactJS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,27 +1,37 @@
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
+  }
+
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if (this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        }
+      },
+      buttonText
+    )
+  }
 }
 
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
-}
-
-let k = 10
-
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
-
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
